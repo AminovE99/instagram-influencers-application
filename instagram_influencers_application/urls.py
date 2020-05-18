5b37c9e4a509ebb969508f2b5c3c8a41cd97968d
Emil <amionv99@gmail.com>
2020-05-18 23:05:08 +0300

    :sparkles: Add Rest architecture
    Signed-off-by: Emil <amionv99@gmail.com>

--- instagram_influencers_application/urls.py
+++ instagram_influencers_application/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hashtags_parse/', views.search_by_tag_view),
+    path('hashtags_parse/', SearchByTagView.as_view())
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,9 +4,21 @@ from django.shortcuts import render
 from webscrape_app.webscrape_util import instascrape
 
 
-def search_by_tag_view(request):
-    if request.POST:
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from webscrape_app.webscrape_util import instascrape
+from rest_framework.views import APIView
+
+
+class SearchByTagView(APIView):
+    def get(self, request):
+        return render(request, "input_page.html")
+
+    def post(self, request):
         data = request.POST.get('search_hashtag')
         instascrape.instascrape.delay(
             "/home/emil/Desktop/instagram_influencers_application/webscrape_app/data/influenc_d.json", 10, data)
-    return render(request, "input_page.html")
+
+        return JsonResponse(data={'status': 'ok'}, status=200)
+
